22dd28bca0915671bc072288b86ac66c3f3222d9
ProstoKuzya <144941155+ProstoKuzya@users.noreply.github.com>
2024-02-18 18:20:13 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import * as ReactDOMClients from 'react-dom/client'
-import Elements from "./Elements.js"
-import "./css/style.css"
-
-const parentNode = ReactDOMClients.createRoot(document.querySelector(".wrapper"))
-
-parentNode.render(<Elements/>)
-
